29ab3aff657676169c85454fe1e57921d07a53b4
noodles <wzp23@126.com>
2018-11-04 10:08:33 +0800

    完成首页
    完成首页信息

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
+import '../node_modules/bootstrap/dist/css/bootstrap.css'
+import './assets/css/index.css'
 
 new Vue({
-  el: '#app',
-  render: h => h(App)
+    el: '#app',
+    render: h => h(App)
 })
